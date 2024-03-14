abb2939fd3fddde99c1f44fbebcad2cb33544fd2
FurkanAkinalp <100966886+FurkanAkinalp@users.noreply.github.com>
2024-03-14 13:12:17 +0000

    Create index.js

---
+++ 30_Days_Of_JavaScript_Exercise/Day_1/index.js
@@ -0,0 +1,34 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+/*
+comments can make code readable,
+ easy to reuse and informative
+*/
+
+let name = 'furkan'
+let age = 45
+let is_true = true
+let none = null
+let lastname
+
+console.log(name, typeof name)
+console.log(age, typeof age)
+console.log(is_true, typeof is_true)
+console.log(none, typeof none)
+console.log(lastname, typeof lastname)
+
+var country
+var city
+var year
+var mail
+
+
+country = 'Turkiye'
+city = 'Ankara'
+year = 2024
+mail = "test@test.com"
+
+console.log(name, age, country, city)
+
+let myAge = 45
+let yourAge = 25
